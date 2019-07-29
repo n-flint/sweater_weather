092c287bfe8a31be23cd3cffca35dc6f62865471
Noah Flint <noah.flint24@gmail.com>
2019-07-29 16:37:50 -0600

    render json from serializer

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -4,7 +4,8 @@ class Api::V1::ForecastController < ApplicationController
   def show
     location = params['location']
     forecast = ForecastFacade.new(location)
-    # render json: ForecastSerializer.new(forecast)
-    require 'pry'; binding.pry
+    # require 'pry'; binding.pry
+    render json: ForecastSerializer.new(forecast.weather)
+    # require 'pry'; binding.pry
   end
 end
