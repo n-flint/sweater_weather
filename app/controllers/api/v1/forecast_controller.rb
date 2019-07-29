b9a91a6324b10b08d0c05ca3e735d25c39c5ed24
Noah Flint <noah.flint24@gmail.com>
2019-07-29 11:47:32 -0600

    added facade.new

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::ForecastController < ApplicationController
   def show
     location = params['location']
     forecast = ForecastFacade.new(location)
+    # render json: ForecastSerializer.new(forecast)
     require 'pry'; binding.pry
   end
 end
