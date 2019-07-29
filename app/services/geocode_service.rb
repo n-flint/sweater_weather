87e2a9e1396fdb524f4c82d88914e23ef117688d
Noah Flint <noah.flint24@gmail.com>
2019-07-29 11:48:12 -0600

    created geocode service

---
+++ app/services/geocode_service.rb
@@ -0,0 +1,19 @@
+class GeocodeService
+
+  def initialize(location)
+    @location = location
+  end
+
+  def get_coordinates
+    service['results'][0]['geometry']['location']
+  end
+
+  private
+
+  def service
+    response = Faraday.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{@location}&key=#{ENV['GOOGLE_API_KEY']}")
+    data = JSON.parse(response.body)
+  end
+
+
+end
