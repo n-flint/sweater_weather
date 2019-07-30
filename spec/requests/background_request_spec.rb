677410682ed39cf3677b499510ce4dd32604723b
Noah Flint <noah.flint24@gmail.com>
2019-07-29 22:02:00 -0600

    created background request spec

---
+++ spec/requests/background_request_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Background Image Request', type: :request do
+  it 'can retrieve a background image for a city' do
+    get '/api/v1/backgrounds?location=denver,co'
+
+    background_data = JSON.parse(response.body)
+
+    expect(response.code).to eq('200')
+    expect(background_data).to have_key('data')
+    expect(background_data['data']['attributes']).to have_key('image')
+  end
+end
+
