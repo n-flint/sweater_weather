104eb6c08e7b4c4488383b6486d49e94d7911c1a
Noah Flint <noah.flint24@gmail.com>
2019-07-30 14:03:41 -0600

    added status 201 to json, password_confirmation to strong params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,12 +3,12 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     facade = UserFacade.new(user_params)
-    render json: UserSerializer.new(facade)
+    render json: UserSerializer.new(facade), status: 201
   end
 
 
   private
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.permit(:email, :password, :password_confirmation)
   end
 end
