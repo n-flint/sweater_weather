2635bb34da7352165f37fae22b51a43120a03bb8
Noah Flint <noah.flint24@gmail.com>
2019-07-30 15:40:30 -0600

    refactored login test

--- spec/requests/login_request_spec.rb
+++ spec/requests/login_request_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Login Request', type: :request do
   it 'can login and recieve an api_key with proper credentials' do
-    existing_user = {
-                "email": "whatever@example.com",
-                "password": "password"
-    }.to_json
+    user = User.create!(email: 'whatever@example.com', password: 'password', password_confirmation: 'password', api_key: 'test-api-key')
+    user_json = {
+                "email": user.email,
+                "password": 'password'
+      }.to_json
 
     headers = { 'CONTENT_TYPE' => 'application/json'}
-    post '/api/v1/sessions', params: existing_user, headers: headers
+    post '/api/v1/sessions', params: user_json, headers: headers
 
     user_key = JSON.parse(response.body)
 
