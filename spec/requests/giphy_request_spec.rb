b50ece57445d1966416f8e26c30b649d61fd286a
Noah Flint <noah.flint24@gmail.com>
2019-07-30 11:58:44 -0600

    refactored expectation data type for time

--- spec/requests/giphy_request_spec.rb
+++ spec/requests/giphy_request_spec.rb
@@ -10,14 +10,8 @@ RSpec.describe 'Giphy Request', type: :request do
     expect(giphy_data).to have_key('data')
 
     expect(giphy_data['data']['attributes']).to have_key('images')
-    expect(giphy_data['data']['attributes']['images'][0]['time']).to be_a(Integer)
+    expect(giphy_data['data']['attributes']['images'][0]['time']).to be_a(String)
     expect(giphy_data['data']['attributes']['images'][0]['summary']).to be_a(String)
     expect(giphy_data['data']['attributes']['images'][0]['url']).to be_a(String)
-
-    # without using fast json api
-    # expect(giphy_data['data']).to have_key('images')
-    # expect(giphy_data['data']['images'][0]['time']).to be_a(Integer)
-    # expect(giphy_data['data']['images'][0]['summary']).to be_a(String)
-    # expect(giphy_data['data']['images'][0]['url']).to be_a(String)
   end
 end
