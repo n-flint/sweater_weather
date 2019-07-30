68a896bc33cb6953f5f86b47d6ab5652f2a6fb0f
Noah Flint <noah.flint24@gmail.com>
2019-07-29 22:29:21 -0600

    app/services/

---
+++ app/services/flickr_service.rb
@@ -0,0 +1,25 @@
+class FlickrService
+
+  def initialize(location)
+    @location = location
+  end
+
+  def get_image
+    JSON.parse(conn)['photos']['photo'][0]
+  end
+
+
+  private
+
+  def conn
+    response = Faraday.get('https://api.flickr.com/services/rest/') do |f|
+      f.params['api_key'] = ENV['FLICKR_API_KEY']
+      f.params['method'] = 'flickr.photos.search'
+      f.params['tags'] = "#{@location},skyline,weather"
+      f.params['text'] = "#{@location}"
+      f.params['nojsoncallback'] = 1
+      f.params['format'] = 'json'
+    end
+    response.body
+  end
+end
