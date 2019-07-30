b658f845068c4de5fa8c83eb5a6b75f9431a5ec1
Noah Flint <noah.flint24@gmail.com>
2019-07-30 13:04:31 -0600

    created user_facade

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -1,7 +1,21 @@
+require 'securerandom'
 class UserFacade
+  attr_reader :id
 
   def initialize(user_data)
-    require 'pry'; binding.pry
+    @id = 1
+    @user_data = user_data
+  end
+
+  def api_key
+    key = SecureRandom.hex(10)
+    create_user(key)
+  end
+
+  private
+
+  def create_user(key)
+    User.create(emal: user_data['email'], password: user_data['password'], api_key: key)
   end
 
 
