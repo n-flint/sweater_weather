92684be13d07d4d431836cd5a97942a3da8d83e5
Noah Flint <noah.flint24@gmail.com>
2019-07-30 09:13:05 -0600

    added route to giphy controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'forecast#show'
       get '/backgrounds', to: 'background#show'
+      get '/gifs', to: 'gifs#index'
     end
   end
 end
