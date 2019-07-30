c45a37b6da12afec874219e0e73b5ec4f266955e
Noah Flint <noah.flint24@gmail.com>
2019-07-30 11:58:03 -0600

    added darksky service test

---
+++ spec/services/darksky_service_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Darksky Service' do
+  it 'retrieves the weather based on coordinates' do
+    coordinates = {
+        "lat"=>39.7392358,
+        "lng"=>-104.990251
+      }
+
+    service = DarkskyService.new(coordinates)
+
+    expect(service.get_weather).to be_a(Hash)
+    expect(service.get_weather).to have_key('currently')
+    expect(service.get_weather).to have_key('hourly')
+    expect(service.get_weather).to have_key('daily')
+    expect(service.get_weather['latitude']).to eq(39.7392358)
+    expect(service.get_weather['longitude']).to eq(-104.990251)
+  end
+end
