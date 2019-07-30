eeb257e13ca2f3a02d0204a14df282883927e886
Noah Flint <noah.flint24@gmail.com>
2019-07-30 16:28:18 -0600

    created road trip request spec

---
+++ spec/requests/road_trip_request_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Road T\rip Request', type: :request do
+  xit 'can receive weather for hour of arrival at destination' do
+    user = User.create!(email: 'whatever@example.com', password: 'password', password_confirmation: 'password', api_key: 'test-api-key')
+    roadtrip_json = {
+                    "origin": "Denver,CO", 
+                    "destination": "Pueblo,CO",
+                    "api_key": "jgn983hy48thw9begh98h4539h4"
+      }.to_json
+
+    headers = { 'CONTENT_TYPE' => 'application/json'}
+    post '/api/v1/road_trip', params: roadtrip_json, headers: headers
+
+    weather_data = JSON.parse(response.body)
+
+    expect(response.code).to eq('200')
+    expect(weather_data).to have_key('data')
+    expect(weather_data['data']['attributes']['time']).to be_a(String)
+    expect(weather_data['data']['attributes']['summary']).to be_a(String)
+    expect(weather_data['data']['attributes']['icon']).to be_a(String)
+    expect(weather_data['data']['attributes']['tempeature']).to be_a(Numeric)
+    expect(weather_data['data']['attributes']['precip_robability']).to be_a(Float)
+    expect(weather_data['data']['attributes']['precip_type']).to be_a(String)
+    expect(weather_data['data']['attributes']['temperature_high']).to be_a(String)
+    expect(weather_data['data']['attributes']['temperature_low']).to be_a(String)
+  end
+end
+
+
