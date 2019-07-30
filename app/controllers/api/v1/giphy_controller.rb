30d6573c4de0d4e0656e79c8cbec7c0a4652e69b
Noah Flint <noah.flint24@gmail.com>
2019-07-30 09:32:17 -0600

    added giphy serializer to index action;

---
+++ app/controllers/api/v1/giphy_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::GiphyController < ApplicationController
+
+
+  def index
+    location = params['location']
+    facade = GiphyFacade.new(params['location'])
+    render json: GiphySerializer.new(facade)
+  end
+end
