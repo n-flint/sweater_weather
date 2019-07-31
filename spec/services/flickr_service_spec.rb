3198b4e70f20b0e6808511c3afe05ccbd283a744
Noah Flint <noah.flint24@gmail.com>
2019-07-31 14:06:31 -0600

    changed method name expectation

--- spec/services/flickr_service_spec.rb
+++ spec/services/flickr_service_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe 'Flickr Service' do
 
         service = FlickrService.new("denver,co")
 
-        expect(service.get_image['owner']).to be_a(String)
-        expect(service.get_image['secret']).to be_a(String)
-        expect(service.get_image['server']).to be_a(String)
-        expect(service.get_image['farm']).to be_a(Integer)
-        expect(service.get_image['title']).to be_a(String)
+        expect(service.image_data['owner']).to be_a(String)
+        expect(service.image_data['secret']).to be_a(String)
+        expect(service.image_data['server']).to be_a(String)
+        expect(service.image_data['farm']).to be_a(Integer)
+        expect(service.image_data['title']).to be_a(String)
     end
   end
 end
