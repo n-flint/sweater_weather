435b841df43f288247a50c1b51c3864d3b9e5a21
Noah Flint <noah.flint24@gmail.com>
2019-07-30 19:55:06 -0600

    refactored forcast method

--- app/facades/roadtrip_facade.rb
+++ app/facades/roadtrip_facade.rb
@@ -8,16 +8,28 @@ class RoadtripFacade
   end
 
   def travel_time
-    service.duration['text']
+    roadtrip_service.duration['text']
   end
 
   def destination_coords
-    service.destination_coords
+    roadtrip_service.destination_coords
+  end
+
+  def forecast
+    current_forecast = DarkskyService.new(destination_coords).get_weather
+    future_forecast = current_forecast['hourly']['data'][hour_offset]
+    # require 'pry'; binding.pry
+    HourlyWeather.new(future_forecast)
   end
 
   private
 
-  def service
+  def hour_offset
+    travel_secs = roadtrip_service.duration['value']
+    ((travel_secs / 3600.0).ceil) - 1
+  end
+
+  def roadtrip_service
     @roadtrip_service = RoadtripService.new(@origin, @destination)
   end
 end
