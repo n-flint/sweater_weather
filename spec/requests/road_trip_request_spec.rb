9ef9512e26f95661083f7b33f8a0ad472ad18ee6
Noah Flint <noah.flint24@gmail.com>
2019-07-30 20:06:47 -0600

    commented out webmock

--- spec/requests/road_trip_request_spec.rb
+++ spec/requests/road_trip_request_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
+# require 'webmock/rspec'
 
-RSpec.describe 'Road T\rip Request', type: :request do
+RSpec.describe 'Road Trip Request', type: :request do
   it 'can receive weather for hour of arrival at destination' do
+    # not sure how to web mock this yet
+    # json_roadtrip_response = File.open("./fixtures/roadtrip.json")
+    # stub_request(:get, "https://maps.googleapis.com/maps/api/directions/json").
+    #     with(
+    #       headers: {
+    #      'Accept'=>'*/*',
+    #      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    #      'User-Agent'=>'Faraday v0.15.4'
+    #       })
+    #       .to_return(status: 200, body: json_roadtrip_response, headers: {})
+
     user = User.create!(email: 'whatever@example.com', password: 'password', password_confirmation: 'password', api_key: 'jgn983hy48thw9begh98h4539h4')
     roadtrip_json = {
                     "origin": "Denver,CO", 
