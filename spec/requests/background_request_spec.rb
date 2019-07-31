7ba4374cc023c666d4a0af919e0ad105ea7181d3
Noah Flint <noah.flint24@gmail.com>
2019-07-31 14:06:17 -0600

    added link expectation to request spec

--- spec/requests/background_request_spec.rb
+++ spec/requests/background_request_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe 'Background Image Request', type: :request do
       expect(response.code).to eq('200')
       expect(background_data).to have_key('data')
       expect(background_data['data']['attributes']).to have_key('image')
+      expect(background_data['data']['attributes']).to have_key('link')
     end
   end
 end
