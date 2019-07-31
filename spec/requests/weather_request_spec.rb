b808f78d61b5cea2cd3be4a66495fdd60ceb1bbb
Noah Flint <noah.flint24@gmail.com>
2019-07-31 10:25:33 -0600

    added more robust expectations;

--- spec/requests/weather_request_spec.rb
+++ spec/requests/weather_request_spec.rb
@@ -10,14 +10,22 @@ RSpec.describe 'Weather Request', type: :request do
 
     expect(response.code).to eq('200')
     expect(weather_data).to have_key('data')
-    expect(weather_data['data']['attributes']).to have_key('current_weather')
+
     expect(weather_data['data']['attributes']['current_weather']['time']).to be_a(Integer)
     expect(weather_data['data']['attributes']['current_weather']['summary']).to be_a(String)
-    
-    #needs more testing
 
-    # expect(weather_data['data']['attributes']['current_weather']['time']).to be_a(Integer)
-    # expect(weather_data['data']['attributes']['current_weather']['time']).to be_a(Integer)
+    expect(weather_data['data']['attributes']['hourly_weather'][0]['time']).to be_a(Integer)
+    expect(weather_data['data']['attributes']['hourly_weather'][0]['icon']).to be_a(String)
+    expect(weather_data['data']['attributes']['hourly_weather'][0]['temperature']).to be_a(Float)
+    expect(weather_data['data']['attributes']['hourly_weather'][0]['summary']).to be_a(String)
+
+    expect(weather_data['data']['attributes']['daily_weather'][0]['time']).to be_a(Integer)
+    expect(weather_data['data']['attributes']['daily_weather'][0]['summary']).to be_a(String)
+    expect(weather_data['data']['attributes']['daily_weather'][0]['icon']).to be_a(String)
+    expect(weather_data['data']['attributes']['daily_weather'][0]['precip_probability']).to be_a(Float)
+    expect(weather_data['data']['attributes']['daily_weather'][0]['precip_type']).to be_a(String)
+    expect(weather_data['data']['attributes']['daily_weather'][0]['temperature_high']).to be_a(Float)
+    expect(weather_data['data']['attributes']['daily_weather'][0]['temperature_low']).to be_a(Float)
     end
   end
 end
